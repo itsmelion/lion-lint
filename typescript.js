8ab8786d8037112109a77bdac762398071926d08
Christhopher Leão <christhopherleao@icloud.com>
2020-08-01 15:52:47 +0100

    prop-types: off

--- typescript.js
+++ typescript.js
@@ -11,6 +11,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'react/prop-types': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
